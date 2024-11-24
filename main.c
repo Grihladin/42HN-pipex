722c6f424548358cdb835f531f844f94eb746c8a
mratke <mratke@student.42heilbronn.de>
2024-11-24 18:39:09 +0100

    no i didnt, it dont want to write.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 16:08:08 by mratke            #+#    #+#             */
-/*   Updated: 2024/11/24 18:21:20 by mratke           ###   ########.fr       */
+/*   Updated: 2024/11/24 18:38:38 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,15 @@ int	main(int argc, char **argv, char **env)
 	char			**path;
 	pid_t			process_id_1;
 	int				pipe_fd[2];
-	pid_t			process_id_2;
+	int				file1_fd;
 
+	// pid_t			process_id_2;
 	j = argc;
 	e = argv;
 	path = get_path(env);
 	cmd1 = parse_cmd(path, "ls");
 	cmd2 = parse_cmd(path, "wc -l");
+	file1_fd = open("file1", O_RDONLY | O_CREAT | O_TRUNC, 777);
 	if (pipe(pipe_fd) == -1)
 	{
 		perror("Pipe creathion error");
@@ -41,27 +43,34 @@ int	main(int argc, char **argv, char **env)
 	}
 	else if (process_id_1 == 0)
 	{
-		dup2(pipe_fd[1], STDOUT_FILENO);
+		dup2(file1_fd, STDOUT_FILENO);
 		close(pipe_fd[0]);
 		close(pipe_fd[1]);
+		close(file1_fd);
 		execve(cmd1.command_path, cmd1.command_paramets, env);
 		perror("Command executhion in child 1 failed");
 		exit(EXIT_FAILURE);
 	}
-	process_id_2 = fork();
-	if (process_id_2 == -1)
-	{
-		perror("Child process 2 creathion failed");
-		exit(EXIT_FAILURE);
-	}
-	else if (process_id_2 == 0)
-	{
-		dup2(pipe_fd[0], STDIN_FILENO);
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
-		execve(cmd2.command_path, cmd2.command_paramets, env);
-		perror("Command executhion in child 2 failed");
-		exit(EXIT_FAILURE);
-	}
+	// process_id_2 = fork();
+	// if (process_id_2 == -1)
+	// {
+	// 	perror("Child process 2 creathion failed");
+	// 	exit(EXIT_FAILURE);
+	// }
+	// else if (process_id_2 == 0)
+	// {
+	// 	dup2(pipe_fd[0], STDIN_FILENO);
+	// 	dup2(file1_fd, STDOUT_FILENO);
+	// 	close(pipe_fd[0]);
+	// 	close(pipe_fd[1]);
+	// 	close(file1_fd);
+	// 	execve(cmd2.command_path, cmd2.command_paramets, env);
+	// 	perror("Command executhion in child 2 failed");
+	// 	exit(EXIT_FAILURE);
+	// }
+	close(pipe_fd[1]);
+	close(pipe_fd[0]);
+	close(file1_fd);
 	waitpid(process_id_1, NULL, 0);
+	// waitpid(process_id_2, NULL, 0);
 }
